20a2758d1421a775cd97f872437c0b9c2a55dab7
Jim Phillips <jim@oberon.group>
2018-10-26 00:26:41 +0000

    Use Dashcore-lib instead of bitcoinjs-lib-dash

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const dash = require('bitcoinjs-lib-dash')
+const dash = require('dashcore-lib')
 const RpcClient = require('bitcoind-rpc');
 var fromHash = function(hash, config) {
   let c;
